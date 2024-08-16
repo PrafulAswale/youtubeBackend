7e6407ec87b1676cf686ccf44b7509dc58eab066
Praful Aswale <prafulcode15@gmail.com>
2024-08-16 14:16:24 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -15,24 +15,6 @@ connectDB()
     console.log("MONGO db connection failed !!!", err);
   });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // .then(() => {
 //     app.listen(process.env.PORT || 8000, () => {
 //         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
